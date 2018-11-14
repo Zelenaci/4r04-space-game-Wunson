951da57d83d32c6761e708df41b84f4aad97b9e6
Roman Svozil <svo35103@spseol.cz>
2018-11-14 17:50:47 +0100

    speed limit

--- objects.py
+++ objects.py
@@ -16,6 +16,7 @@ class SpaceObject(object):
         self.winx = 0 # v okne
         self.winy = 0 # -//-
         self.sprite = self.load_image(img_file)
+        self.rotation = 0
 
         self.hitbox = range((self.x - self.sprite.width // 2), (self.x + self.sprite.width // 2))
         self.hitboy = range((self.y - self.sprite.height // 2), (self.y + self.sprite.height // 2))
@@ -31,9 +32,11 @@ class SpaceObject(object):
         self.winy = self.y - (player.y - window.height // 2)
         self.sprite.x = self.winx
         self.sprite.y = self.winy
-        self.sprite.rotation = self.direction
-        
+        self.sprite.rotation = self.rotation
+
     def move(self, dt):
+        self.speed = self.maxspeed if self.speed > self.maxspeed else self.speed
+        self.speed = 0 if self.speed < 0 else self.speed
         self.x += dt * self.speed * cos(pi/2 - radians(self.direction))
         self.y += dt * self.speed * sin(pi/2 - radians(self.direction))
 
@@ -44,6 +47,7 @@ class PlayerShip(SpaceObject):
         super().__init__(img_file, x, y)
         self.direction = 0
         self.speed = 0
+        self.maxspeed = 500
         self.thrust = 5         # zrychlení
         self.deceleration = 1   # zpomalení
         self.rspeed = 5         # rychlost otáčení
@@ -68,8 +72,10 @@ class PlayerShip(SpaceObject):
                 self.speed -= self.thrust
             elif key == 97:
                 self.direction -= self.rspeed
+                self.rotation -= self.rspeed
             elif key == 100:
                 self.direction += self.rspeed
+                self.rotation += self.rspeed
 
     def tick(self, dt):
         self.control()
@@ -83,15 +89,11 @@ class EnemyShip(SpaceObject):
 """__________________________________________METEOR_________________________"""
 class Meteor(SpaceObject):
 
-    def __init__(self, img_file, x, y, speed=0, rspeed=0):
+    def __init__(self, img_file, x, y):
         super().__init__(img_file, x, y)
-        self.direction = 0
-        self.speed = speed
-        self.rspeed = rspeed
         pyglet.clock.schedule_interval(self.tick, 1 / fps)
 
     def tick(self, dt):
-        self.move(dt)
         self.refresh()
 
 """_______________________________EVENTS____________________________________"""
@@ -117,8 +119,6 @@ rojmeteoru = []
 for x in range(0, 100):
     x = randint(0, 1000)
     y = randint(0, 1000)
-    speed = randint(0, 10)
-    rspeed = randint(0, 5)
-    meteor = Meteor('PNG/Meteors/meteorBrown_big1.png', x, y, speed, rspeed)
+    meteor = Meteor('PNG/Meteors/meteorBrown_big1.png', x, y)
     
 pyglet.app.run()
