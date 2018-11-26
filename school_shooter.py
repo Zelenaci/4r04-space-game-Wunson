a3fe18e7bcaeae128943bb9e67bbfc826310e3d8
Ondřej Wünsch <wunsch.ondrej@gmail.com>
2018-11-26 20:52:35 +0100

    šipečky

--- school_shooter.py
+++ school_shooter.py
@@ -7,6 +7,15 @@ window = pyglet.window.Window(1300, 700)
 batch = pyglet.graphics.Batch()
 keys = []
 
+"____________________________________functions__________________________________"
+def overlap(hitbox1, hitbox2):
+    return max(0, min(hitbox1[1], hitbox2[1]) - max(hitbox1[0], hitbox2[0]))
+
+def load_image(path):
+        load = pyglet.image.load(path)
+        load.anchor_x = load.width // 2
+        load.anchor_y = load.height // 2
+        return pyglet.sprite.Sprite(load, batch = batch)
 "____________________________________classes__________________________________"
 class SpaceObject(object):
 
@@ -16,27 +25,22 @@ class SpaceObject(object):
         self.x = x
         self.y = y
         self.mass = mass
+        self.drag = 0.989
         
-        self.sprite = self.load_image(img_file)
+        self.sprite = load_image(img_file)
         self.sprite.x = self.x
         self.sprite.y = self.y
         
-        self.hitbox = range(int(self.x - self.sprite.width // 2), int(self.x + self.sprite.width // 2))
-        self.hitboy = range(int(self.y - self.sprite.height // 2), int(self.y + self.sprite.height // 2))
-
-    def load_image(self, path):
-        load = pyglet.image.load(path)
-        load.anchor_x = load.width // 2
-        load.anchor_y = load.height // 2
-        return pyglet.sprite.Sprite(load, batch = batch)
+        self.hitbox = (int(self.x - self.sprite.width // 2), int(self.x + self.sprite.width // 2))
+        self.hitboy = (int(self.y - self.sprite.height // 2), int(self.y + self.sprite.height // 2))
     
     def refresh(self):
         self.sprite.x = self.x
         self.sprite.y = self.y
         self.sprite.rotation = degrees(self.rotation - (pi /2))
     
-    def thrst(self, thrust):
-        new_vector = self.vector + complex(thrust*cos(self.rotation), thrust*sin(self.rotation)) 
+    def burn(self):
+        new_vector = self.vector + complex(self.thrust*cos(self.rotation), self.thrust*sin(self.rotation)) 
         if abs(new_vector) < 750:
             self.vector = new_vector
     
@@ -47,14 +51,20 @@ class SpaceObject(object):
         if self.y > window.height or self.y < 0:
             self.vector = complex(self.vector.real, -self.vector.imag)
             
-        if self.x > window.width+15 or self.x < -15:
-            self.x = window.width -15  
+        if self.x > window.width+15:
+            self.x = 1
         
-        if self.y > window.height+15 or self.y < -15:
-            self.y = window.height - 15
+        if self.x < -15:
+            self.x = window.width - 1
+        
+        if self.y > window.height+15:
+            self.y = 1
+        
+        if self.y < -15:
+            self.y = window.height - 1
         
     def move(self, dt):
-        self.vector *= 0.98
+        self.vector *= self.drag
         self.x -= dt * self.vector.real
         self.y += dt * self.vector.imag
         
@@ -66,16 +76,7 @@ class Meteor(SpaceObject):
         super().__init__(img_file, x, y, mass)
     
     def get_hit(self):
-        if abs(self.x - player.x) < self.sprite.width and abs(self.y - player.y) < self.sprite.height:
-            mass = self.mass + player.mass
-            
-            force = abs(self.vector + player.vector) / mass
-            
-            temp = player.vector * force * self.mass 
-            
-            player.vector = self.vector * force * player.mass
-            
-            self.vector = temp
+        pass
     
     def tick(self, dt):
         self.get_hit()
@@ -102,15 +103,18 @@ class PlayerShip(SpaceObject):
            A = 97
            D = 100
            spc = 32"""
-           
+         
+        if 115 in keys or 65364 in keys:
+            self.drag = 0.95
+        else:
+            self.drag = 0.989
+         
         for key in keys:
-            if key == 119: #W
-                self.thrst(25)   
-            elif key == 115:
-                pass
-            elif key == 97:
+            if key == 119 or key == 65362: #W
+                self.burn()   
+            elif key == 97 or key == 65361:
                 self.rotation -= self.rspeed
-            elif key == 100:
+            elif key == 100 or key == 65363:
                 self.rotation += self.rspeed
                 
     def tick(self, dt):
@@ -122,17 +126,17 @@ class PlayerShip(SpaceObject):
 class Missile(SpaceObject):    
     def __init__(self, img_file, x, y, mass):
         super().__init__(img_file, x, y, mass)
+        self.thrust = 25
         
-    def aim(self, coordinates):
-        x = coordinates[0] - self.x
-        y = coordinates[1] - self.y
+    def aim(self):
+        x = player.x - self.x
+        y = player.y - self.y
         self.rotation = atan2(y, -x)
-        print(degrees(self.rotation))    
         
     def tick(self, dt):
-        self.thrst(30)
+        self.burn()
         self.move(dt)
-        self.aim(player.get_coordinates())
+        self.aim()
         self.refresh()
     
 "_________________________________events______________________________________"
@@ -143,6 +147,7 @@ def on_draw():
     
 @window.event
 def on_key_press(key, mod):
+    print(key)
     keys.append(key)    
 
 @window.event
